d196846b58bda5f6c2d8c12f147d975d85b10141
Jonathan Soto <jonathansoto.an@gmail.com>
2016-02-02 20:00:56 -0600

    Add basic project structure, the solver class and its test class

---
+++ src/com/caletre/quadraticequation/service/SimpleQuadraticEquationSolver.java
@@ -0,0 +1,19 @@
+package com.caletre.quadraticequation.service;
+
+import com.caletre.quadraticequation.model.QuadraticSolution;
+
+/**
+ * Represents a solver for the quadratic equation that does not take into account
+ * complex numbers.
+ * @author Jonathan Soto
+ * @see SimpleQuadraticEquationSolver
+ */
+public class SimpleQuadraticEquationSolver implements QuadraticEquationSolver {
+
+	@Override
+	public QuadraticSolution solve(int a, int b, int c) {
+		// TODO Implement this method
+		return null;
+	}
+
+}
---
+++ test/com/caletre/quadraticequation/test/SimpleQuadraticEquationSolverTest.java
@@ -0,0 +1,12 @@
+package com.caletre.quadraticequation.test;
+
+import org.junit.Test;
+
+// TODO Implement all the test cases for the SimpleQuadraticEquationSolver class
+public class SimpleQuadraticEquationSolverTest {
+	
+	@Test
+	public void testABeingZeroThrowsAnException(){
+ 
+	}
+}
